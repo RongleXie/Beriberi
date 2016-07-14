726e76d40fb1f4a2813d878cb8e7bcfbc3da84b5
xieqingrong <252254002@qq.com>
2016-07-14 16:42:41 +0800

    add web files

---
+++ head.js
@@ -0,0 +1,3 @@
+﻿document.write("<script src='hs.js'></script>");
+document.write("<script src='1.js'></script>");
+document.write("<script src='add.js'></script>");
---
+++ web/dzdm.js
@@ -0,0 +1,28 @@
+﻿document.write("<div id='dzdm'  style='display:none;'>"+dzdm+"</div>");
+if(g("Ccode"))
+{
+  g("Ccode").value = g("dzdm").innerHTML;
+
+}
+
+if(g("ShowAd"))
+{
+	
+g("ShowAd").innerHTML = g("dzdm").innerHTML;
+if(document.getElementsByName("ShowAd"))
+{
+  momoObj = document.getElementsByName("ShowAd");
+	
+  for(i=0; i<momoObj.length; i++)
+  {
+    momoObj[i].innerHTML = g("dzdm").innerHTML;
+  }
+}
+
+
+
+document.write("<style><!--");
+document.write("#ShowAd{filter:glow(color=#FF0080,strength=2);color:#FFFFFF;font-size:28px;font-family:Arial, Helvetica, sans-serif;width:100%;text-align:center;}");
+document.write("--></style>");
+
+}
